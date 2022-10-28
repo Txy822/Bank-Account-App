bfe8848f94b0f25342593c420509d095a238386b
tesfahun <y.w.tesfahun@gmail.com>
2022-10-28 17:41:24 +0100

    Test class added

---
+++ app/src/test/java/com/tes/eat/anywhere/bankaccountapp/AccountTest.kt
@@ -0,0 +1,17 @@
+package com.tes.eat.anywhere.bankaccountapp
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+
+class AccountTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
